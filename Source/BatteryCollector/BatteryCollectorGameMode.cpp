ca29eb7522f8a1bfc91ccc0f414cdff1f167f9b9
Robert Achmann <achmann@outlook.com>
2016-02-10 00:47:57 -0400

    Up to start of 19
    Everything compiles up to start of step 19

--- Source/BatteryCollector/BatteryCollectorGameMode.cpp
+++ Source/BatteryCollector/BatteryCollectorGameMode.cpp
@@ -4,6 +4,7 @@
 #include "BatteryCollectorGameMode.h"
 #include "BatteryCollectorCharacter.h"
 #include "Kismet/GameplayStatics.h"
+#include "Runtime/UMG/Public/Blueprint/UserWidget.h"
 
 ABatteryCollectorGameMode::ABatteryCollectorGameMode()
 {
@@ -18,6 +19,29 @@ ABatteryCollectorGameMode::ABatteryCollectorGameMode()
     DecayRate = 0.01f;
 }
 
+void  ABatteryCollectorGameMode::BeginPlay()
+{
+    Super::BeginPlay();
+    
+    SetCurrentState(EBatteryPlayState::EPlaying);
+    
+    // Set the score to beat to win
+    ABatteryCollectorCharacter* MyCharacter = Cast<ABatteryCollectorCharacter>(UGameplayStatics::GetPlayerPawn(this,0));
+    if(MyCharacter)
+    {
+        PowerToWin = (MyCharacter->GetInitialPower())*1.25f;
+    }
+    
+    if(HUDWidgetClass != nullptr)
+    {
+        CurrentWidget = CreateWidget<UUserWidget>(GetWorld(), HUDWidgetClass);
+        if (CurrentWidget != nullptr)
+        {
+            CurrentWidget->AddToViewport();
+        }
+    }
+}
+
 void ABatteryCollectorGameMode::Tick(float DeltaTime)
 {
     Super::Tick(DeltaTime);
@@ -26,13 +50,34 @@ void ABatteryCollectorGameMode::Tick(float DeltaTime)
     ABatteryCollectorCharacter* MyCharacter = Cast<ABatteryCollectorCharacter>(UGameplayStatics::GetPlayerPawn(this,0));
     if(MyCharacter)
     {
+        // if power is greater than needed to win, set the game's state to Won
+        if(MyCharacter->GetCurrentPower() > PowerToWin)
+        {
+            SetCurrentState(EBatteryPlayState::EWon);
+        }
         // if the character's poer is positive
-        if(MyCharacter->GetCurrentPower()>0)
+        else if(MyCharacter->GetCurrentPower()>0)
         {
             // decrease the character's power
             MyCharacter->UpdatePower(-DeltaTime*DecayRate*(MyCharacter->GetInitialPower()));
         }
+        else {
+            SetCurrentState(EBatteryPlayState::EGameOver);
+        }
     }
-    
-    
+}
+
+float ABatteryCollectorGameMode::GetPowerToWin() const
+{
+    return PowerToWin;
+}
+
+EBatteryPlayState ABatteryCollectorGameMode::GetCurrentState() const
+{
+    return CurrentState;
+}
+
+void ABatteryCollectorGameMode::SetCurrentState(EBatteryPlayState NewState)
+{
+    CurrentState = NewState;
 }
--- Source/BatteryCollector/BatteryCollectorGameMode.h
+++ Source/BatteryCollector/BatteryCollectorGameMode.h
@@ -3,22 +3,65 @@
 #include "GameFramework/GameMode.h"
 #include "BatteryCollectorGameMode.generated.h"
 
+
+//enum to store the current state of gameplay
+UENUM(BlueprintType)
+enum class EBatteryPlayState
+{
+    EPlaying,
+    EGameOver,
+    EWon,
+    EUnknown
+};
+
 UCLASS(minimalapi)
 class ABatteryCollectorGameMode : public AGameMode
 {
-	GENERATED_BODY()
+    GENERATED_BODY()
+    
 
 public:
 	ABatteryCollectorGameMode();
     
     virtual void Tick(float DeltaTime) override;
     
+    // returns power needed to win - needed for the HUD
+    UFUNCTION(BlueprintPure, Category = "Power")
+    float GetPowerToWin() const;
+    
+    virtual void BeginPlay() override;
+    
+    // Returns the current playing state
+    UFUNCTION(Blueprintpure, Category = "Power")
+    EBatteryPlayState GetCurrentState() const;
+    
+    // Sets a new playing state
+    void SetCurrentState(EBatteryPlayState NewState);
+    
 protected:
     // Game Mode - this file - sets the rules of the game...
     //
-    // the rate at which the character loses power
-    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Power")
+    // The rate at which the character loses power
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Power", Meta = (BlueprintProtected = "true"))
     float DecayRate;
+    
+    // The power needed to win the game
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Power", Meta = (BlueprintProtected = "true"))
+    float PowerToWin;
+    
+    // The widget class to use for the HUD screen
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Power", Meta = (BlueprintProtected = "true"))
+    TSubclassOf<class UUserWidget> HUDWidgetClass;
+    
+    // The Actual Instance of the HUD
+    UPROPERTY()
+    class UUserWidget* CurrentWidget;
+    
+private:
+    
+    // keeps track of current play state
+    EBatteryPlayState CurrentState;
+    
 };
 
 
